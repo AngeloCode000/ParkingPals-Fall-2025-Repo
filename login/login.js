b5499324c94e4da430b3d18b008b42be984a1b55
CGhost613 <cjacobi0613@gmail.com>
2025-11-06 21:21:37 -0500

    added return button to login page

--- login/login.js
+++ login/login.js
@@ -2,6 +2,8 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
+const returnForm = document.getElementById("return-form");
+const returnButton = document.getElementById("return-form-submit");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -20,4 +22,11 @@ loginButton.addEventListener("click", (e) => {
     else {
         loginErrorMsg.style.opacity = 1;
     }
+})
+
+returnButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("Redirecting to Home page");
+    window.location.href = '/ParkingPals-Fall-2025-Repo/index.html';
+    //location.reload();
 })
